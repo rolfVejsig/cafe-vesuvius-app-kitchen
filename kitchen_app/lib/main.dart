8d61074faff09a39bf8869488f9e0e6b9739cf23
Skole computer <rolfvejsig@gmail.com>
2025-09-30 08:44:08 +0200

    kitchen yaaay

--- kitchen_app/lib/main.dart
+++ kitchen_app/lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'dart:io';
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
+}
 
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const KitchenApp());
 }
