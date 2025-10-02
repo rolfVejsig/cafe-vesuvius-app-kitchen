11b57320440b566868c82bc16e6fd3ad85b25e2f
Skole computer <rolfvejsig@gmail.com>
2025-10-02 08:14:19 +0200

    virker pweeeease

---
+++ kitchen_app/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\HFGF\\Desktop\\cafe-vesuvius-app-kitchen\\kitchen_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\HFGF\\Desktop\\cafe-vesuvius-app-kitchen\\kitchen_app"
+  "FLUTTER_ROOT=C:\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\HFGF\\Desktop\\cafe-vesuvius-app-kitchen\\kitchen_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\HFGF\\Desktop\\cafe-vesuvius-app-kitchen\\kitchen_app"
+  "FLUTTER_TARGET=C:\\Users\\HFGF\\Desktop\\cafe-vesuvius-app-kitchen\\kitchen_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzUuMQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049MjBmODI3NDkzOQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MWU5YTgxMWJmOA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjA="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\HFGF\\Desktop\\cafe-vesuvius-app-kitchen\\kitchen_app\\.dart_tool\\package_config.json"
+)
